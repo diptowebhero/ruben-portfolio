058926656262ea4bfe74bfd742babb1ee24c5a7c
diptowebhero <diptodas1672@gmail.com>
2025-01-25 15:44:50 +0600

    feat: add portfolio section

--- js/main.js
+++ js/main.js
@@ -21,3 +21,34 @@ document.addEventListener("click", (event) => {
     mobileMenu.classList.remove("active");
   }
 });
+
+// JavaScript for handling modal data
+document.querySelectorAll(".image-gallery img").forEach((img) => {
+  img.addEventListener("click", function () {
+    const modalImage = document.getElementById("modalImage");
+    const modalDescription = document.getElementById("imageDescription");
+    modalImage.src = this.src;
+    modalImage.alt = this.alt;
+    modalDescription.textContent = this.getAttribute("data-description");
+  });
+});
+// Add fade-in effect for images
+document.querySelectorAll(".tab-pane").forEach((tab) => {
+  // When a tab is shown
+  tab.addEventListener("shown.bs.tab", () => {
+    const images = tab.querySelectorAll("img");
+    images.forEach((img, index) => {
+      setTimeout(() => {
+        img.classList.add("show");
+      }, index * 300); // Adjust the delay (300ms) for smoother animation
+    });
+  });
+
+  // When a tab is hidden
+  tab.addEventListener("hidden.bs.tab", () => {
+    const images = tab.querySelectorAll("img");
+    images.forEach((img) => {
+      img.classList.remove("show");
+    });
+  });
+});
