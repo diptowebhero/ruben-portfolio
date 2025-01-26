49d5d190b3918d97bcb0f60a2e0a1b3ac7a596bf
diptowebhero <diptodas1672@gmail.com>
2025-01-26 14:22:12 +0600

    update: portfolio section updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+$(window).on("load", function () {
+  $(".grid").isotope({
+    itemSelector: ".grid-item",
+    masonry: {
+      gutter: 20,
+    },
+  });
+
+  // filter items on button click
+  $(".filter-button-group").on("click", "li", function () {
+    var filterValue = $(this).attr("data-filter");
+    $(".grid").isotope({ filter: filterValue });
+    $(".filter-button-group li").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
 const menuIcon = document.getElementById("menuIcon");
 const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobile_menu");
@@ -20,35 +37,4 @@ document.addEventListener("click", (event) => {
   if (!isClickInside) {
     mobileMenu.classList.remove("active");
   }
-});
-
-// JavaScript for handling modal data
-document.querySelectorAll(".image-gallery img").forEach((img) => {
-  img.addEventListener("click", function () {
-    const modalImage = document.getElementById("modalImage");
-    const modalDescription = document.getElementById("imageDescription");
-    modalImage.src = this.src;
-    modalImage.alt = this.alt;
-    modalDescription.textContent = this.getAttribute("data-description");
-  });
-});
-// Add fade-in effect for images
-document.querySelectorAll(".tab-pane").forEach((tab) => {
-  // When a tab is shown
-  tab.addEventListener("shown.bs.tab", () => {
-    const images = tab.querySelectorAll("img");
-    images.forEach((img, index) => {
-      setTimeout(() => {
-        img.classList.add("show");
-      }, index * 300); // Adjust the delay (300ms) for smoother animation
-    });
-  });
-
-  // When a tab is hidden
-  tab.addEventListener("hidden.bs.tab", () => {
-    const images = tab.querySelectorAll("img");
-    images.forEach((img) => {
-      img.classList.remove("show");
-    });
-  });
 });
